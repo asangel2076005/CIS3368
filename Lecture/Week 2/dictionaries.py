7cb0c7d2f69bf0bb3dff184123d563282d5f20f7
asangel2076005 <asangel@cougarnet.uh.edu>
2024-01-27 11:32:22 -0600

    echo "# CIS3368" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/asangel2076005/CIS3368.git git push -u origin main

--- Lecture/Week 2/dictionaries.py
+++ Lecture/Week 2/dictionaries.py
@@ -13,3 +13,25 @@ print(my_dict["first_name"])
 
 first_name = my_dict.get("first_name")
 print(first_name)
+print()
+
+# Reassignment
+my_dict = {
+    "first_name": "Jane",
+    "last_name": "Doe",
+    "birth_year": 2001,
+    "children": ["Jack", "John", "James"]
+}
+
+print(my_dict)
+print(my_dict["children"][0])
+print()
+
+# Note: the old my_dict object is garbage collected since it is reassigned by a new my_dict object
+
+# Updating dictionaries
+my_dict["birth_year"] = 2000
+print(my_dict)
+
+my_dict.update({"birth_year": 1900})
+print(my_dict)
