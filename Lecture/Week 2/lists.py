78f17c271588b2b2a542bafd7c0b0ab34c58c148
asangel2076005 <asangel@cougarnet.uh.edu>
2024-01-27 12:06:16 -0600

    echo "# CIS3368" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/asangel2076005/CIS3368.git git push -u origin main

--- Lecture/Week 2/lists.py
+++ Lecture/Week 2/lists.py
@@ -1,1 +1,32 @@
 # Lists
+# they are ordered, changeable, allow duplications and multiple data types
+
+my_list = ["apple", "kiwi", "pear"]
+print(my_list)
+print()
+
+# Reassigning
+my_list = ["apple", "kiwi", "pear", "apple", "apple"]
+print(my_list)
+print()
+
+# In this case, the old my_list is garbage collected and replaced by the new my_list
+
+# count the list item amount
+list_length = len(my_list)
+print(list_length)
+print()
+
+# Data types in lists
+my_list = [100, 15, 77, 99, 300]
+print(my_list)
+
+my_list = [True, False, True]
+print(my_list)
+
+my_list = ["Apple", 300, True, 400, "Alex", [1, 3, 5, 7, 9]]
+print(my_list)
+print()
+
+# In conclusion, lists can contain many data types but has overhead
+
