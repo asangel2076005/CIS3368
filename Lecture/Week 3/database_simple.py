cd6520f0f22ef9e263953ca8547559dc3d159c03
asangel2076005 <asangel@cougarnet.uh.edu>
2024-02-03 12:36:39 -0600

    Update database_simple.py

---
+++ Lecture/Week 3/database_simple.py
@@ -0,0 +1,31 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_con(hostname, username, user_password, dbname):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=hostname,
+            user=username,
+            password=user_password,
+            databse=dbname
+        )
+        print("Connection Success")
+
+    except Error as e:
+        print(f"The error {e} occurred")
+
+    return connection
+
+
+if __name__ == "__main__":
+    conn = create_con("cis3368spring.cb0eqkk045ol.us-east-1.rds.amazonaws.com",
+                      "angeloangel",
+                      "serdon17",
+                      "cis3368springdb")
+
+    cursor = conn.cursor(dictionary=True)
+    sql = "SELECT * FROM users"  # String we want to deliver
+
+    cursor.execute(sql)
