dffa8d6781a6db0febc91656fefd41a254c0d5f6
asangel2076005 <asangel@cougarnet.uh.edu>
2024-02-10 11:52:15 -0600

    Added week 4 files

--- Lecture/Week 4/crudops.py
+++ Lecture/Week 4/crudops.py
@@ -1,6 +1,10 @@
 import mysql.connector
 import creds
 from mysql.connector import Error
-from sql_helper import create_connection
-from sql_helper import execute_query
-from sql_helper import execut_read_query
+from sql import create_connection
+from sql import execute_query
+from sql import execut_read_query
+
+# Create a connection to MySQL database
+my_creds = creds.Creds()
+print(my_creds)
