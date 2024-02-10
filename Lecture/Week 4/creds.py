32f1ed64df99d7773c799ca2158e89215ffa73a0
asangel2076005 <asangel@cougarnet.uh.edu>
2024-02-10 11:50:59 -0600

    Added week 4 files

---
+++ Lecture/Week 4/creds.py
@@ -0,0 +1,6 @@
+class Creds:
+    connection_string = "cis3368spring.cb0eqkk045ol.us-east-1.rds.amazonaws.com"
+    user_name = "angeloangel"
+    password = "angelopassword"
+    database_name = "CIS3368spring"
+
---
+++ Lecture/Week 4/crudops.py
@@ -0,0 +1,6 @@
+import mysql.connector
+import creds
+from mysql.connector import Error
+from sql_helper import create_connection
+from sql_helper import execute_query
+from sql_helper import execut_read_query
