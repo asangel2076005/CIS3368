1918ce1513c79c3ca13c442c6b310364268f6f41
asangel2076005 <122799955+asangel2076005@users.noreply.github.com>
2024-03-01 23:35:33 -0600

    Delete Playground/creds.py

--- Playground/creds.py
+++
@@ -1,5 +0,0 @@
-class Creds:
-    connection_string = "cis3368spring.cb0eqkk045ol.us-east-1.rds.amazonaws.com"
-    user_name = "angeloangel"
-    password = "angelopassword"
-    database_name = "cis3368springdb"
