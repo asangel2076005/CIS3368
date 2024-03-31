8a97d2db1c41921027f176659f40a60d80ddb288
asangel2076005 <juuzousuzuya200@gmail.com>
2024-03-31 14:49:51 -0500

    ADD

---
+++ Playground/EJSPractice/UI/server.js
@@ -0,0 +1,52 @@
+// load the things we need
+var express = require('express');
+var app = express();
+const bodyParser  = require('body-parser');
+
+// required module to make calls to a REST API
+const axios = require('axios');
+app.use(bodyParser.urlencoded());
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+
+
+// Start here
+app.get('/', function(req, res) {
+
+    //itunes API call
+    axios.get('https://itunes.apple.com/search?term=radiohead')
+    .then((response)=>{
+        let musicData = response.data;
+        console.log(musicData);
+        res.render('pages/index', {
+            music: musicData
+        });
+    });
+
+});
+
+// About
+app.get("/about", function(req, res) {
+    //local API call to my Python REST API that delivers cars
+    axios.get(`http://127.0.0.1:5000/api/student`)
+    .then((response)=>{
+        
+        var student = response.data;
+        var tagline = "Here is the data coming from my own API";
+        console.log(student);
+         // use res.render to load up an ejs view file
+        res.render('pages/about', {
+            student: student,
+            tagline: tagline
+        });
+    });
+});
+
+
+
+// Start the express application on port 8080 and print server start message
+const port = 8080;
+app.listen(port, () => console.log("Application started and listening on port 8080"));
